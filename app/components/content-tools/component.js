848a6abdb1f5090ca3f392cba12d081128a44797
Ivo van der Wijk <github@in.m3r.nl>
2016-10-30 18:01:51 +0100

    start addon

---
+++ app/components/content-tools/component.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-contenttools/components/content-tools/component';
--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-contenttools'
+  name: 'ember-contenttools',
+  isDevelopingAddon: function() { return true; }
 };
